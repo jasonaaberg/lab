9b7492dbfac61a994cf925a1e9a79f24a689a3c9
aaberg@gmail.com <aaberg@gmail.com>
2023-09-25 22:20:37 -0500

    Variables bruh

---
+++ learning_variables.py
@@ -0,0 +1,20 @@
+
+
+s1 = "Hello World! "
+s2 = "Today is a good day. "
+s3 = "I am learning Python."
+
+HelloWorld = s1 + s2 + s3
+
+print(HelloWorld)
+
+n1 = 10
+n2 = 20
+i1 = "Pancakes"
+i2 = "Waffles"
+s4 = "I like to eat "
+s5 = " for breakfast."
+s6 = s4 + i1 + s5 + " Pancakes cost $" + str(n1) + " and waffles cost $" + str(n2) + "."
+
+Breakfast = s6
+print(Breakfast)
